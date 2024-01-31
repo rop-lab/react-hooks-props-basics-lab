7b5c0a40cff006fba63459abf7c00fb9ac925ee6
cheruiyotrop <cheruiyot.rop@student.moringaschool.com>
2024-01-31 14:55:29 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,19 @@
-import React from "react";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import About from "./About";
+import React from 'react';
+import NavBar from './NavBar';
+import Home from './Home';
+import About from './About';
 
-// pass this data down as props to the child component(s) that need it!
-import user from "../data/user";
-console.log(user);
+import user from '../data/user';
+console.log(user)
+function App (){
+    return (
+      <div>
+        <NavBar />
+        <Home name={user.name} city ={user.city} color = {user.color} />
+        <About bio={user.bio} github={user.links.github} linkedin={user.links.linkedin} />
+      </div>
+    );
+    }
 
-function App() {
-  return (
-    <div>
-      <NavBar />
-      <Home />
-      <About />
-    </div>
-  );
-}
 
 export default App;
